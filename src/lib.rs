91a7287880c8577f85379e54d1a646bf140baf8c
Ian Knight <ian.knight2@nottingham.ac.uk>
2024-01-06 13:06:40 +0000

    Rough implementation of traits for carry and borrow

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
+pub mod carry_borrow;
 pub mod z80;
 use z80::Z80;
 
 /// Constrain a function to run for a minimum length of time.
-/// 
+///
 /// This macro relies on [`std::time::Instant`] for timing and expects a [`std::time::Duration`]
 /// to indicate the duration to last. Both these structs should be used in every module
 /// calling this macro.
 ///
 /// # Examples
 /// These examples use an imagined function `foo` which adds two `u8`s.
-/// 
+///
 /// Basic example forcing a call to `foo` to last at least 1000ns:
 /// ```
 /// # #[macro_use] extern crate spectrum;
@@ -17,10 +18,10 @@ use z80::Z80;
 /// #
 /// # fn foo(x: u8, y: u8) -> u8 { x + y }
 /// # fn main() {
-/// time_limit!(foo(2, 3); Duration::from_nanos(1000)); 
+/// time_limit!(foo(2, 3); Duration::from_nanos(1000));
 /// # }
 /// ```
-/// 
+///
 /// The time limited function can still return its normal return value:
 /// ```
 /// # #[macro_use] extern crate spectrum;
