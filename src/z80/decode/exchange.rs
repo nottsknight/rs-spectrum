243c8c9880157755f18771ec67ddb34bd48d0ad5
Ian Knight <ian.knight2@nottingham.ac.uk>
2024-01-09 09:37:58 +0000

    Refactor exchange decode

--- src/z80/decode/exchange.rs
+++ src/z80/decode/exchange.rs
@@ -1,7 +1,7 @@
-use super::{Instr, DecodeResult};
+use super::{DecodeResult, Instr};
 
 /// Attempt to decode an Exchange, Transfer, or Search instruction.
-/// 
+///
 /// # Arguments
 /// - `mem`: slice of memory with the instruction to decode beginning at `mem[0]`
 pub fn exchange(mem: &[u8]) -> DecodeResult {
@@ -12,14 +12,17 @@ pub fn exchange(mem: &[u8]) -> DecodeResult {
         [0xe3, ..] => Some((Instr::EX_SP_HL, 1)),
         [0xdd, 0xe3, ..] => Some((Instr::EX_SP_IX, 2)),
         [0xfd, 0xe3, ..] => Some((Instr::EX_SP_IY, 2)),
-        [0xed, 0xa0, ..] => Some((Instr::LDI, 2)),
-        [0xed, 0xb0, ..] => Some((Instr::LDIR, 2)),
-        [0xed, 0xa8, ..] => Some((Instr::LDD, 2)),
-        [0xed, 0xb8, ..] => Some((Instr::LDDR, 2)),
-        [0xed, 0xa1, ..] => Some((Instr::CPI, 2)),
-        [0xed, 0xb1, ..] => Some((Instr::CPIR, 2)),
-        [0xed, 0xa9, ..] => Some((Instr::CPD, 2)),
-        [0xed, 0xb9, ..] => Some((Instr::CPDR, 2)),
-        _ => None
+        [0xed, n, ..] => match *n {
+            0xa0 => Some((Instr::LDI, 2)),
+            0xb0 => Some((Instr::LDIR, 2)),
+            0xa8 => Some((Instr::LDD, 2)),
+            0xb8 => Some((Instr::LDDR, 2)),
+            0xa1 => Some((Instr::CPI, 2)),
+            0xb1 => Some((Instr::CPIR, 2)),
+            0xa9 => Some((Instr::CPD, 2)),
+            0xb9 => Some((Instr::CPDR, 2)),
+            _ => None,
+        },
+        _ => None,
     }
 }
